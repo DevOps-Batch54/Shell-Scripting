e347e88b9a57e95b162de17e31140874570967c6
unknown <maheshhr88505@gmail.com>
2023-08-29 01:04:35 +0530

    Auto Push

--- 05-SpecialVaribles.sh
+++ 05-SpecialVaribles.sh
@@ -4,3 +4,9 @@ echo Name of the script executed is $0
 echo "Name of the Trainer is $1"
 echo "Training Batch number is $2"
 echo "current topic is $3"
+
+echo $*
+echo $@
+echo $$
+echo $#
+echo $?
