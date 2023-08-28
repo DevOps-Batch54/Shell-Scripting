60eadd5df994f501cde106a369325e77db94a803
unknown <maheshhr88505@gmail.com>
2023-08-29 01:50:05 +0530

    Auto Push

--- 08-Function.sh
+++ 08-Function.sh
@@ -4,4 +4,13 @@ Sample(){
     echo "I am executing sample function"
     echo -e "\e[35m Sample function completed \e[0m"
 }
-Sample
+
+Status(){
+    echo "Good Morning and todays date is $(date +%D)"
+    echo -e "Good Morning and Todays Date is \e[34m $(date +%F) \e[0m"
+    echo "No Of Open Sessions $No_Of_Sessions" \e[0m"
+}
+#This is how you can call a function
+Sample 
+#Calling status function
+Status
