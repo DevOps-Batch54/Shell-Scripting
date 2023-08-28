2117e67c539d59f9cc83d3e678443d56970c5ad0
unknown <maheshhr88505@gmail.com>
2023-08-28 16:37:03 +0530

    Auto Push

--- 01.Shibang.sh
+++ 01.Shibang.sh
@@ -1,11 +1,17 @@
 #!bin/bash
 '''
 # Start of the line in any bash script should always be a SHEBANG NOTATION.
+
 # Apart from the first line, none of the lines are considered as shebang.
+
 # Shebang notation should always start with #
+
 # Using this SHEBANG Notation we are telling the system the interpretor to use to run this script.
+
 # Your server would have java, go, python, nodejs, shell installed.
+
 # Now if you run the script, How system will come to know which interpretor it has to use,That where Shebang Notations hepls.
+
 '''
 
 echo Welcome To batch54 Cloud Devops Training..
