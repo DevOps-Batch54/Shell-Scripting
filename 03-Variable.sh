5d62906d8c8fd81eb308dd9e6d12a10d5c82f4c6
unknown <maheshhr88505@gmail.com>
2023-08-28 19:20:23 +0530

    Auto Push

--- 03-Variable.sh
+++ 03-Variable.sh
@@ -4,6 +4,6 @@ b=20;
 c=30;
 echo $a
 echo -e Valur of the variable of a is "\e[32m $a \e[0m"
-echo -e Value of the variable of b is ${b}
-echo -e Value of the variable of b is ${c}''
-echo -e Value of the variable of d is $d
+echo -e Value of the variable of b is "\e[34m $b \e[0m"
+echo -e Value of the variable of b is "\e[36m $c \e[0m"
+echo -e Value of the variable of d is "\e[35m $d \e[0m"
