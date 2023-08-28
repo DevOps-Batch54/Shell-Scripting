720210f809ef846b985df33c3cd3253d37326ead
unknown <maheshhr88505@gmail.com>
2023-08-29 01:22:10 +0530

    Auto Push

--- 06-Read.sh
+++ 06-Read.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-read -p "Enter Your Name :"Name
+read -p "Enter Your Name :" Name
 echo -e "Name of the entered value is /e[32m $Name /e[0m"
 echo -e "Name of the entered value is $Name"
