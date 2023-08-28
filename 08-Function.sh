b51530234ed2ac34967669f864f30fedd9dd2bcc
unknown <maheshhr88505@gmail.com>
2023-08-29 01:55:12 +0530

    Auto Push

--- 08-Function.sh
+++ 08-Function.sh
@@ -8,7 +8,7 @@ Sample(){
 Status(){
     echo "Good Morning and todays date is $(date +%D)"
     echo -e "Good Morning and Todays Date is \e[34m $(date +%F) \e[0m"
-    echo "No Of Open Sessions \e[35m $(who --q) \e[0m"
+    echo "No Of Open Sessions \e[35m $(who -q) \e[0m"
 }
 #This is how you can call a function
 Sample 
