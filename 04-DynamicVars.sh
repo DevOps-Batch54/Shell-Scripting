a46876628eb9d57cf34ad04449027eba0ef40dc0
unknown <maheshhr88505@gmail.com>
2023-08-28 23:59:59 +0530

    Auto Push

--- 04-DynamicVars.sh
+++ 04-DynamicVars.sh
@@ -2,4 +2,4 @@
 TODAYS_DATE="03JUNE2023"
 echo "Good Morning and todays date is $TODAYS_DATE"
 TODAY_DATE=$(date +%F)
-echo -e "Good Morning and Todays Date is \e [34m $TODAY_DATE \e [0m"
+echo -e "Good Morning and Todays Date is "\e [34m $TODAY_DATE \e [0m""
