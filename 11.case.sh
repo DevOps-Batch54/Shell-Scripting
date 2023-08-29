5abcd9d7ed7130a89532c89a7b078504ad4ed922
unknown <maheshhr88505@gmail.com>
2023-08-29 19:43:09 +0530

    Auto Push

--- 11.case.sh
+++ 11.case.sh
@@ -9,14 +9,18 @@ ACTION=$1
 case $ACTION in
     start)
         echo -e "\e[32m Starting The RabbitMQ Services \e[0m"
+        exit 0
         ;;
     stop)
         echo -e "\e[31m Stoping The RabbitMQ Services \e[0m"
+        exit 1
         ;;
     restart)
         echo -e "\e[33m Restarting The RabbitMQ Services \e[0m"
+        exit 2
         ;;
     *)
         echo -e "\e[35m Possible Values are start or stop or restart \e[0m"
+        exit 3
         ;;
 esac
