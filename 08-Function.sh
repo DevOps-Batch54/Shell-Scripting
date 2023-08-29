daa4a85a5f6c1cbb74f294c7af4938cf38d99142
unknown <maheshhr88505@gmail.com>
2023-08-29 17:17:50 +0530

    Auto Push

--- 08-Function.sh
+++ 08-Function.sh
@@ -12,6 +12,7 @@ Status(){
     echo -e "Good Morning and Todays Date is \e[34m $(date +%F) \e[0m"
     echo -e "No Of Open Sessions \e[35m $(who -q) \e[0m"
     echo -e "No Of Open Sessions \e[35m $(who | wc -l) \e[0m"
+    echo "Load Average On The System From Last 1 min is $(uptime | awk -F , '{print $3}' | awk -F : '{print$2}')"
 }
 #This is how you can call a function
 Sample 
