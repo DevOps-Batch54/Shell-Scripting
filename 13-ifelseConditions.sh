954cda9899455a56e3386377d0693353122a2602
unknown <maheshhr88505@gmail.com>
2023-08-31 19:59:14 +0530

    Auto Push

--- 13-ifelseConditions.sh
+++ 13-ifelseConditions.sh
@@ -14,3 +14,22 @@ else
         echo -e "\e[32m Available Option is Start only \e[0m"
         exit 1
 fi
+
+#else-if
+ACTION=$1
+if [ "$ACTION" == "start" ]; then
+        echo -e "\e[32m Starting The RabbitMQ Services \e[0m"
+        exit 0
+elif [ "$ACTION" == "restart" ]; then
+        echo -e "\e[34m Restarting The RabbitMQ Services \e[0m"
+        exit 1
+elif [ "$ACTION" == "stop" ]; then
+        echo -e "\e[36m Stopping The RabbitMQ Services \e[0m"
+        exit 2
+elif [ "$ACTION" == "Enable" ]; then
+        echo -e "\e[38m Enable The RabbitMQ Services \e[0m"
+        exit 3
+else 
+        echo -e "\e[35m Available Option is Start----Restart----Stop----Enable only \e[0m"
+        exit 4
+fi
