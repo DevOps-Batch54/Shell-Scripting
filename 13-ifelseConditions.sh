fb819499d8f2538c2494d3e478c8a8a1fa27488f
unknown <maheshhr88505@gmail.com>
2023-08-31 20:00:46 +0530

    Auto Push

--- 13-ifelseConditions.sh
+++ 13-ifelseConditions.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #Simple if
-ACTION=$1
-if [ "$ACTION" == "start" ]; then
-        echo -e "\e[32m Starting The RabbitMQ Services \e[0m"
-        #exit 0
-fi
-#if-else
-ACTION=$1
-if [ "$ACTION" == "start" ]; then
-        echo -e "\e[32m Starting The RabbitMQ Services \e[0m"
-        exit 0
-else 
-        echo -e "\e[32m Available Option is Start only \e[0m"
-        exit 1
-fi
+# ACTION=$1
+# if [ "$ACTION" == "start" ]; then
+#         echo -e "\e[32m Starting The RabbitMQ Services \e[0m"
+#         #exit 0
+# fi
+# #if-else
+# ACTION=$1
+# if [ "$ACTION" == "start" ]; then
+#         echo -e "\e[32m Starting The RabbitMQ Services \e[0m"
+#         exit 0
+# else 
+#         echo -e "\e[32m Available Option is Start only \e[0m"
+#         exit 1
+# fi
 
 #else-if
 ACTION=$1
