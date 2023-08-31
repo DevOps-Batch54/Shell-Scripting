60b93916e6fbb314938083f1ec897ec536c0def3
unknown <maheshhr88505@gmail.com>
2023-08-31 19:55:03 +0530

    Auto Push

--- 13-ifelseConditions.sh
+++ 13-ifelseConditions.sh
@@ -7,7 +7,7 @@ if [ "$ACTION" == "start" ]; then
 fi
 #if-else
 ACTION=$1
-if ["$ACTION" == "start"]; then
+if [ "$ACTION" == "start" ]; then
         echo -e "\e[32m Starting The RabbitMQ Services \e[0m"
         exit 0
 else 
