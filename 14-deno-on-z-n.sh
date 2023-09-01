aed282f3c9d0de7a9609a64b5ca3664f4be4c899
unknown <maheshhr88505@gmail.com>
2023-09-01 12:41:27 +0530

    Auto Push

--- 14-deno-on-z-n.sh
+++ 14-deno-on-z-n.sh
@@ -2,13 +2,13 @@
 
 a=10
 
-if [ -n. "$var"]; then
+if [ -n. "$var"] ; then
     echo "Value of a is declared"
 else
     echo "Value of a is not declared"
 fi
 
-if [ -z. "$var"]; then
+if [ -z. "$var"] ; then
     echo "Value of b is declared"
 else
     echo "Value of b is not declared"
