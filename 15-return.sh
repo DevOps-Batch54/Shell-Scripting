aa4e5b7360473dd7332d09cb401b53ace40805c1
unknown <maheshhr88505@gmail.com>
2023-09-01 13:04:14 +0530

    Auto Push

---
+++ 15-return.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sample(){
+    echo "I am messaged called from the function"4
+
+}
+stat(){
+echo " \t total no.of sessions $(who | wc -l)"
+echo " todays Date is $(date +%F)"
+echo "\t stat function completed"
+echo " Calling sample function"
+# exit 2
+return
+sample
+}
+stat
+echo -e "\t\t stat and sample functions are completed"
