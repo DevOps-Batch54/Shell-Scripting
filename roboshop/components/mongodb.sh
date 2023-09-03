33a5a99760f941079601d8f2b0616d3939bfccee
unknown <maheshhr88505@gmail.com>
2023-09-03 15:30:37 +0530

    Auto Push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -8,8 +8,8 @@ if [ $ID -ne 0 ] ; then
     exit 1;
 fi
 
-stat () {
-if [ $? -eq 0 ] ; then 
+stat() {
+if [ $1 -eq 0 ] ; then 
     echo -e "\e[33m Success \e[0m"
         else
     echo -e "\e[31m Failure \e[0m"
