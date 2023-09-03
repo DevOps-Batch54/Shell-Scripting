5bb817c02b51e83492a6953cd47fe082ca2ae26b
unknown <maheshhr88505@gmail.com>
2023-09-03 16:59:43 +0530

    Auto Push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -19,7 +19,7 @@ fi
 echo -e "*******/e[32m Catalogue installation started \e[0m*******"
 echo -n "Download the nodesource"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y
+yum install nodejs -y &>> LOGFILE
 stat $?
 
 # curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
