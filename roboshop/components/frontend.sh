dc4faaafabed34f7d8e8ad9ea2b0358363d6bcb3
unknown <maheshhr88505@gmail.com>
2023-09-03 12:29:39 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-echo "I am Frontend"
+#echo "I am Frontend"
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+echo -m "This script is expected to be run by a root user or with a sudo previlage"
+exit 1;
+fi
+echo "Installing Nginx"
+yum install mginx -y
+
+#It's always a great idea to preform validation before you get an exception.
+#if the script is exectuted as a root user or a sudo user, then it as to proceed.
+#if not, I was to exit the script with some nice message.
+
+# The frontend is the service in RobotShop to serve the web content over Nginx.
+
+# Install Nginx.
+
+# ```
+# # yum install nginx -y
+# # systemctl enable nginx
+# # systemctl start nginx
+
+# ```
+
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
+# ```
+# # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# ```
+
+# Deploy in Nginx Default Location.
