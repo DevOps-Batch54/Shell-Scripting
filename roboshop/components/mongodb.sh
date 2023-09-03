ad16c6c48ad69ccc8cd50b3c21a472d6a001a81e
unknown <maheshhr88505@gmail.com>
2023-09-03 15:33:33 +0530

    Auto Push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -19,17 +19,12 @@ fi
 echo -n "Configuring the $mongodb repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
+echo -n "Installing $COMPONENT"
+yum install -y mongodb-org &>> LOGFILE
+stat $?
 
 
-# 1. Setup MongoDB repos.
 
-# ```bash
-# # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
-# ```
-
-# 1. Install Mongo & Start Service.
-
-# ```bash
 # # yum install -y mongodb-org
 # # systemctl enable mongod
 # # systemctl start mongod
