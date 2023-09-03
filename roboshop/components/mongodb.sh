bef60d3d8ddeb50452a1a049a4d9d3b3c12cfd44
unknown <maheshhr88505@gmail.com>
2023-09-03 15:21:51 +0530

    Auto Push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,2 +1,40 @@
 #!/bin/bash
-echo "I am Mongodb"
+#echo "I am Mongodb"
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo previlage \e[0m"
+    exit 1;
+fi
+
+stat () {
+if [ $1 -eq 0 ] ; then 
+    echo -e "\e[33m Success \e[0m"
+        else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2;
+fi
+}
+echo -n "Configuring the $mongodb repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+# 1. Setup MongoDB repos.
+
+# ```bash
+# # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+# ```
+
+# 1. Install Mongo & Start Service.
+
+# ```bash
+# # yum install -y mongodb-org
+# # systemctl enable mongod
+# # systemctl start mongod
+
+# ```
+
+# 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
+
+# Config file:   `# vim /etc/mongod.conf`
