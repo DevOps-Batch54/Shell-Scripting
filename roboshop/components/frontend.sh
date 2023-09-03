bc9d6cab7e62c62a52a444f46bb86bdea2ef49dc
unknown <maheshhr88505@gmail.com>
2023-09-03 13:39:01 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,31 +3,28 @@
 COMPONENT=frontend
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
-echo -e "\e[31m This script is expected to be run by a root user or with a sudo previlage \e[0m"
-exit 1;
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo previlage \e[0m"
+    exit 1;
 fi
-echo -n "Installing Nginx :"
-yum install nginx -y &>> "/tmp/${COMPONENT}.log"
-if [ $? -eq 0 ] ; then 
+
+stat () {
+if [ $1 -eq 0 ] ; then 
     echo -e "\e[33m Success \e[0m"
         else
     echo -e "\e[31m Failure \e[0m"
+    exit 2;
 fi
+}
+echo -n "Installing Nginx :"
+yum install nginx -y &>> "/tmp/${COMPONENT}.log"
+stat $?
 echo -n "Downloading the ${COMPONENT} component"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-if [ $? -eq 0 ] ; then
-    echo -e "\e[33m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
 echo -n "Performing Cleanup :"
 cd /usr/share/nginx/html
 rm -rf * &>> "/tmp/${COMPONENT}.log"
-if [ $? -eq 0 ] ; then
-    echo -e "\e[33m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
 
 # cd /usr/share/nginx/html
 # rm -rf *
