48cbb5609289f9a1537837fda0a8be3488cfe98c
unknown <maheshhr88505@gmail.com>
2023-09-03 13:07:30 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -18,7 +18,7 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend
 if [ $? -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
 else
-    echo -e \e[31m Failure \e[0m"
+    echo -e "\e[31m Failure \e[0m"
 fi
 
 
