25a9eefcc9fb8981546c945da4fe25a83c85ff2e
unknown <maheshhr88505@gmail.com>
2023-09-03 12:12:38 +0530

    Auto Push

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo $0
-bash components/$1.sh
+
+echo $0   #o/p: wrapper.sh
+bash components/$1.sh 
 bash components/$2.sh
 bash components/$3.sh 
+
+#bash wrapper.sh catalogue mongodb frontend
+#o/p: 
+#wrapper.sh
+#I am catalogue
+#I am mongodb
+#I am frontend
