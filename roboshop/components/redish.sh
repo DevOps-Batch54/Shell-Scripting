667b4f54f896014ce98c8e7492838d74a609a3ec
unknown <maheshhr88505@gmail.com>
2023-09-05 14:33:58 +0530

    Auto push

--- roboshop/components/redish.sh
+++ roboshop/components/redish.sh
@@ -4,7 +4,7 @@ COMPONENT=redis
 source components/common.sh
 
 echo -n "Download the $COMPONENT repo"
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -s -o https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 yum install redis-6.2.13 -y &>> LOGFILE
 stat $?
 
