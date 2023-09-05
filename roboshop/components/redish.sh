28789f8d616c32756a667c928bf3f95a7ae9ed5a
unknown <maheshhr88505@gmail.com>
2023-09-05 14:56:36 +0530

    Auto push

--- roboshop/components/redish.sh
+++ roboshop/components/redish.sh
@@ -1,22 +1,6 @@
-#!bin/bash
-
+#!bon/bash
 COMPONENT=redis
 source components/common.sh
-
 echo -n "Download the $COMPONENT repo"
-rm ./-L
-#curl -o -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo
-#/etc/yum.repos.d/redis.repo
-#yum install redis-6.2.13 -y &>> LOGFILE
-stat $?
-
-# echo -n "Enabling the DB Visibility:"
-# sed -i -e 's/bind 127.0.0.1 -/bind 0.0.0.0 -/' /etc/redis.conf
-# stat $?
-
-# vim /etc/redis.conf
-# vim /etc/redis/redis.conf
-
-# systemctl enable redis
-# systemctl start redis
-# systemctl status redis -l
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+# yum install redis-6.2.11 -y
