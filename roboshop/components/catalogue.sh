4ba34e18be724ee18632b3a7f95a997cac71e499
unknown <maheshhr88505@gmail.com>
2023-09-05 18:50:27 +0530

    Auto Push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,46 +4,4 @@ COMPONENT=catalogue
 
 source components/common.sh
 
-echo -e "*******\e[32m Catalogue installation started \e[0m*******"
-echo -n "Configure the $COMPONENT repo "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> LOGFILE
-stat $?
-echo -n "Installing the NODEJS:"
-yum install nodejs -y &>> LOGFILE
-stat $?
-id $APPUSER &>> LOGFILE
-if [ $? -ne 0 ] ; then
-echo -n "Creating the service account"
-useradd $APPUSER &>> LOGFILE
-stat $?
-fi
-echo -n "Downloading the $COMPONENT component:"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
-stat $?
-echo -n "Copying the $COMPONENT to $APPUSER home directory :"
-cd /home/$APPUSER
-rm -rf $COMPONENT &>> LOGFILE
-unzip -o /tmp/$COMPONENT.zip &>> LOGFILE
-stat $?
-
-echo -n "Modifying the ownership :"
-mv $COMPONENT-main $COMPONENT &>> LOGFILE
-chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT                        
-cd /home/$APPUSER/$COMPONENT
-npm install -y &>> LOGFILE
-stat $?
-
-
-
-echo -n "Update the mongodb IP address:"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
-stat $?
-echo -n "update the systemd file as $COMPONENT file"
-#chown -R /home/$APPUSER/$COMPONENT $APPUSER:$APPUSER 
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $?
-echo -n "Start the $COMPONENT"
-systemctl daemon-reload &>> LOGFILE
-systemctl enable $COMPONENT &>> LOGFILE
-systemctl restart $COMPONENT &>> LOGFILE
-stat $?
+NODEJS         # Call the function for catalogue
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -16,14 +16,58 @@ if [ $1 -eq 0 ] ; then
 fi
 }
 
-CREATE_USER(){
-    id $APPUSER &>> LOGFILE
-if [ $? -ne 0 ] ; then
-echo -n "Creating the service account"
-useradd $APPUSER &>> LOGFILE
-stat $?
-fi
+DOWNLOAD_AND_EXTRACT(){
+        echo -n "Downloading the $COMPONENT component:"
+        curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+        stat $?
+        echo -n "Copying the $COMPONENT to $APPUSER home directory :"
+        cd /home/$APPUSER
+        rm -rf $COMPONENT &>> LOGFILE
+        unzip -o /tmp/$COMPONENT.zip &>> LOGFILE
+        stat $?
+
+        echo -n "Modifying the ownership :"
+        mv $COMPONENT-main $COMPONENT &>> LOGFILE
+        chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT                        
+        stat $?
+        
 }
+
+CREATE_USER(){
+        id $APPUSER &>> LOGFILE
+        if [ $? -ne 0 ] ; then
+        echo -n "Creating the service account"
+        useradd $APPUSER &>> LOGFILE
+        stat $?
+        fi
+}
+
+NPM_INSTALL(){
+
+        echo -n "Generating npm $COMPONENT artifactory :"
+        cd /home/$APPUSER/$COMPONENT
+        npm install -y &>> LOGFILE
+        stat $?
+
+}
+
+CONFIGURE_SERVICE(){
+
+        echo -n "Update the mongodb IP address:"
+        sed -i -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+        stat $?
+        echo -n "update the systemd file as $COMPONENT file"
+        #chown -R /home/$APPUSER/$COMPONENT $APPUSER:$APPUSER 
+        mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+        stat $?
+        echo -n "Start the $COMPONENT"
+        systemctl daemon-reload &>> LOGFILE
+        systemctl enable $COMPONENT &>> LOGFILE
+        systemctl restart $COMPONENT &>> LOGFILE
+        stat $?
+
+}
+
 
 NODEJS(){
         echo -n "Configure the $COMPONENT repo "
@@ -32,4 +76,7 @@ NODEJS(){
         echo -n "Installing the NODEJS:"
         yum install nodejs -y &>> LOGFILE
         stat $?
+        CREATE_USER            #calling create user function to create the roboshop user account
+        DOWNLOAD_AND_EXTRACT    # calling DOWNLOAD_AND_EXTRACT function to download the content
+        NPM_INSTALL             # calling NPM_INSTALL function to creates artifactory
 }
