f846326c94bd2d0315d9ac22464f9eec6aaf40ee
unknown <maheshhr88505@gmail.com>
2023-09-05 17:01:08 +0530

    Auto Push

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
-echo "I am Cart"
+COMPONENT=cart
+source components/common.sh
+
+echo -n "Configure the $COMPONENT repo "
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> LOGFILE
+stat $?
+echo -n "Installing the NODEJS:"
+yum install nodejs -y &>> LOGFILE
+stat $?
+id $APPUSER &>> LOGFILE
+if [ $? -ne 0 ] ; then
+echo -n "Creating the service account"
+useradd $APPUSER &>> LOGFILE
+stat $?
+fi
+echo -n "Downloading the $COMPONENT component:"
+curl -s -L -o /tmp/cart.zip "https://github.com/stans-robot-project/cart/archive/main.zip"
+stat $?
+echo -n "Copying the $COMPONENT to $APPUSER home directory :"
+cd /home/$APPUSER
+rm -rf $COMPONENT &>> LOGFILE
+unzip -o /tmp/$COMPONENT.zip &>> LOGFILE
+stat $?
+
+echo -n "Modifying the ownership :"
+mv $COMPONENT-main $COMPONENT &>> LOGFILE
+chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT                        
+cd /home/$APPUSER/$COMPONENT
+npm install -y &>> LOGFILE
+stat $?
